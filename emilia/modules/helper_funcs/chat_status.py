5f693498ea6ec85b7747e10276bda921351f007d
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2022-01-06 14:24:03 +0700

    initial commit

--- emilia/modules/helper_funcs/chat_status.py
+++ emilia/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from EmikoRobot import (
+from emilia import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from EmikoRobot.modules import connection
+from emilia.modules import connection
 
 connected = connection.connected
