46b903b70e27518fc144a25c09aee393e02299f8
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2022-01-06 18:46:41 +0700

    initial commit

--- emilia/modules/reporting.py
+++ emilia/modules/reporting.py
@@ -264,17 +264,6 @@ def buttons(update: Update, context: CallbackContext):
             query.answer("🛑 Failed to delete message!")
 
 
-__help__ = """
-❂ /report <reason>*:* reply to a message to report it to admins.
-❂ @admin*:* reply to a message to report it to admins.
-*NOTE:* Neither of these will get triggered if used by admins.
-
-*Admins only:*
-❂ /reports <on/off>*:* change report setting, or view current status.
-❂ If done in pm, toggles your status.
-❂ If in group, toggles that groups's status.
-"""
-
 SETTING_HANDLER = CommandHandler("reports", report_setting, run_async=True)
 REPORT_HANDLER = CommandHandler(
     "report", report, filters=Filters.chat_type.groups, run_async=True
