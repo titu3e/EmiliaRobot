253567ec11c772bd8bb7a098d2209b07c6884691
ZenitsuID #M•R•T™ <85629049+ZenitsuID@users.noreply.github.com>
2022-01-06 18:27:52 +0700

    initial commit

--- emilia/modules/tagall.py
+++ emilia/modules/tagall.py
@@ -110,13 +110,3 @@ async def cancel_spam(event):
 
 
 __mod_name__ = "Tag all"
-__help__ = """
-──「 Mention all func 」──
-
-Emiko Can Be a Mention Bot for your group.
-
-Only admins can tag all.  here is a list of commands
-
-❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
-❂ /cancel for canceling the mention-all.
-"""
