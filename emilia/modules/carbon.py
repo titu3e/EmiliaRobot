210c0ef3e64f8b0ea91a04800088d06fee704f92
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-13 03:40:00 +0700

    Update carbon.py

--- emilia/modules/carbon.py
+++ emilia/modules/carbon.py
@@ -24,15 +24,15 @@ async def carbon_func(_, message):
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/11b5922a33de9968cedfe.jpg"
+MEMEK = "https://telegra.ph/file/7cd17e6f15a68275a33a5.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emiko Robot** 
+        caption=f"""✨ **Hey I'm Emilia** 
 
-**Owner repo : [sena-ex](https://t.me/excrybaby)**
+**Owner repo : [ZenitsuPrjkt](https://t.me/ZenitsuPrjkt)**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
@@ -44,9 +44,9 @@ async def repo(_, message):
             [
                 [
                     InlineKeyboardButton(
-                        "Repo", url="https://github.com/kennedy-ex/emikorobot"), 
+                        "Repo", url="https://github.com/ZenitsuPrjkt/Emilia"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/emikosupport")
+                        "Support", url="https://t.me/EmiliaSupport")
                 ]
             ]
         )
