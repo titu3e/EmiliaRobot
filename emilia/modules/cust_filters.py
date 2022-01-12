18ec2a7a2763e57851dc7f2d5c1727a04752f8b7
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-13 01:52:18 +0700

    Update cust_filters.py

--- emilia/modules/cust_filters.py
+++ emilia/modules/cust_filters.py
@@ -607,11 +607,13 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-❂ /filters*:* List all active filters saved in the chat.
-*Admin only:*
-❂ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
+✦ *User Command:*
+✧ /filters*:* List all active filters saved in the chat.
+
+✦ *Admin only:*
+✧ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'
+is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter
+keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you
 doin?
  Separate diff replies by `%%%` to get random replies
  *Example:* 
@@ -621,13 +623,13 @@ doin?
  Reply 2
  %%%
  Reply 3`
+✧ /stop <filter keyword>*:* Stop that filter.
 
-❂ /stop <filter keyword>*:* Stop that filter.
+✦ *Chat creator only:*
+✧ /removeallfilters*:* Remove all chat filters at once.
 
-*Chat creator only:*
-
-❂ /removeallfilters*:* Remove all chat filters at once.
-*Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
+✦ *Note*:
+Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check /markdownhelp to know more!
 """
 
