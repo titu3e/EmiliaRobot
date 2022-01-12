0908d08d56d8528e6c676723c7eab37ba0a18ab0
Zenitsu Projects™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-12 19:55:34 +0700

    Update alive.py

--- emilia/modules/alive.py
+++ emilia/modules/alive.py
@@ -9,16 +9,16 @@ from emilia.events import register
 from emilia import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/915afe0954eba90060387.jpg"
+PHOTO = "https://telegra.ph/file/f39bb53399e4c4bfaa9db.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emilia.** \n\n"
   TEXT += "**I'm Working Properly** \n\n"
-  TEXT += f"**My Master : [ZenitsuID](https://t.me/ZenitsuID)** \n\n"
+  TEXT += f"**My Master : [ZenitsuPrjkt](https://t.me/ZenitsuPrjkt)** \n\n"
   TEXT += f"**Library Version :** `{telever}` \n\n"
   TEXT += f"**Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"**Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here️**"
-  BUTTON = [[Button.url("Help", "https://t.me/EmiliaPrjkt_bot?start=help"), Button.url("Repo", "https://github.com/ZenitsuID/Emilia-Prjkt")]]
+  BUTTON = [[Button.url("Help", "https://t.me/EmiliaPrjkt_bot?start=help"), Button.url("Repo", "https://github.com/ZenitsuPrjkt/Emilia")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
