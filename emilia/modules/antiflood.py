03a2096f8da7ae447567224cd391e9de6e3cd9a9
Zenitsu Projects™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-12 20:12:05 +0700

    Update antiflood.py

--- emilia/modules/antiflood.py
+++ emilia/modules/antiflood.py
@@ -401,11 +401,11 @@ __help__ = """
 You know how sometimes, people join, send 100 messages, and ruin your chat? With antiflood, that happens no more!
 Antiflood allows you to take action on users that send more than x messages in a row. Actions are: ban/kick/mute
 
-*Admin commands:*
-- /flood*:* Get the current antiflood settings.
-- /setflood <number/off/no>*:* Set the number of messages after which to take action on a user. Set to '0', 'off', or 'no' to disable.
-- /setfloodmode <action type>*:* Choose which action to take on a user who has been flooding. Options: ban/kick/mute
-- /delflood <yes/no/on/off>*:* If you want bot to delete messages flooded by user.
+✦*Admin commands:*
+✧ /flood*:* Get the current antiflood settings.
+✧ /setflood <number/off/no>*:* Set the number of messages after which to take action on a user. Set to '0', 'off', or 'no' to disable.
+✧ /setfloodmode <action type>*:* Choose which action to take on a user who has been flooding. Options: ban/kick/mute.
+✧ /delflood <yes/no/on/off>*:* If you want bot to delete messages flooded by user.
 """
 
 __mod_name__ = "Anti-Flood"
