815e3e7a6cb70923b9a77006f9444f7c9f1e27dc
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-15 15:45:52 +0700

    Add LICENSE

--- emilia/modules/carbon.py
+++ emilia/modules/carbon.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2022 Zenitsu-Project.
+#
+# Emilia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Emilia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# translate to Indonesian by @ZenitsuPrjkt
+
 from platform import python_version as y
 from telegram import __version__ as o
 from pyrogram import __version__ as z
@@ -13,24 +30,24 @@ from emilia.utils.functions import make_carbon
 @capture_err
 async def carbon_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
+        return await message.reply_text("`Membalas pesan teks untuk membuat karbon.`")
     if not message.reply_to_message.text:
-        return await message.reply_text("`Reply to a text message to make carbon.`")
-    m = await message.reply_text("`Preparing Carbon`")
+        return await message.reply_text("`Membalas pesan teks untuk membuat karbon.`")
+    m = await message.reply_text("`Mempersiapkan Karbon`")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("`Uploading`")
+    await m.edit("`Mengunggah`")
     await pbot.send_photo(message.chat.id, carbon)
     await m.delete()
     carbon.close()
 
 
-MEMEK = "https://telegra.ph/file/7cd17e6f15a68275a33a5.jpg"
+MEMEK = "https://telegra.ph/file/8142e7aac030eebd40a4e.jpg"
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=MEMEK,
-        caption=f"""✨ **Hey I'm Emilia** 
+        caption=f"""✨ **Hei saya Emilia** 
 
 **Owner repo : [ZenitsuPrjkt](https://t.me/ZenitsuPrjkt)**
 **Python Version :** `{y()}`
@@ -46,7 +63,7 @@ async def repo(_, message):
                     InlineKeyboardButton(
                         "Repo", url="https://github.com/ZenitsuPrjkt/Emilia"), 
                     InlineKeyboardButton(
-                        "Support", url="https://t.me/EmiliaSupport")
+                        "Grup", url="https://t.me/EmiliaSupport")
                 ]
             ]
         )
