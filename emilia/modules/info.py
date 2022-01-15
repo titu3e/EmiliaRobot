bcfd0e708039a7ca8c922f1e010947e1410dd07b
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-15 16:01:23 +0700

    Change to Indonesian as default

--- emilia/modules/info.py
+++ emilia/modules/info.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2022 Zenitsu-Project.
+#
+# Emilia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Emilia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# translate to Indonesian by @ZenitsuPrjkt
+
 import os
 
 from pyrogram import filters
@@ -12,7 +29,7 @@ async def get_user_info(user, already=False):
     if not already:
         user = await app.get_users(user)
     if not user.first_name:
-        return ["Deleted account", None]
+        return ["Akun yang dihapus", None]
     user_id = user.id
     username = user.username
     first_name = user.first_name
@@ -28,7 +45,7 @@ async def get_user_info(user, already=False):
         "Mention": [mention],
         "Sudo": is_sudo,
     }
-    caption = section("User info", body)
+    caption = section("Info pengguna", body)
     return [caption, photo_id]
 
 
@@ -58,7 +75,7 @@ async def get_chat_info(chat, already=False):
         "Restricted": is_restricted,
         "Description": [description],
     }
-    caption = section("Chat info", body)
+    caption = section("Info", body)
     return [caption, photo_id]
 
 
@@ -71,7 +88,7 @@ async def info_func(_, message: Message):
     elif not message.reply_to_message and len(message.command) != 1:
         user = message.text.split(None, 1)[1]
 
-    m = await message.reply_text("Processing...")
+    m = await message.reply_text("Memuat...")
 
     try:
         info_caption, photo_id = await get_user_info(user)
@@ -104,7 +121,7 @@ async def chat_info_func(_, message: Message):
         elif len(message.command) == 2:
             chat = message.text.split(None, 1)[1]
 
-        m = await message.reply_text("Processing...")
+        m = await message.reply_text("Memuat...")
 
         info_caption, photo_id = await get_chat_info(chat)
         if not photo_id:
