9fb9a8fb6cc47c7be60336b9306c70ed0619e170
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-16 11:52:17 +0700

    Change to Indonesian as default

--- emilia/modules/webshot.py
+++ emilia/modules/webshot.py
@@ -32,18 +32,18 @@ async def take_ss(_, message: Message):
     try:
         if len(message.command) != 2:
             return await message.reply_text(
-                "Give A Url To Fetch Screenshot."
+                "Berikan Url Untuk Mengambil Tangkapan Layar."
             )
         url = message.text.split(None, 1)[1]
-        m = await message.reply_text("**Taking Screenshot**")
-        await m.edit("**Uploading**")
+        m = await message.reply_text("**Mengambil Tangkapan Layar**")
+        await m.edit("**Mengunggah**")
         try:
             await message.reply_photo(
                 photo=f"https://webshot.amanoteam.com/print?q={url}",
                 quote=False,
             )
         except TypeError:
-            return await m.edit("No Such Website.")
+            return await m.edit("Tidak Ada Situs Web Tersebut.")
         await m.delete()
     except Exception as e:
         await message.reply_text(str(e))
