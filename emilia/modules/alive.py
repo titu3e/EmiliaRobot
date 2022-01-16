0536cceee17baf7e45d5de5d00e7176d4fa6a995
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-16 17:25:56 +0700

    Add files via upload

--- emilia/modules/alive.py
+++ emilia/modules/alive.py
@@ -26,15 +26,14 @@ from emilia.events import register
 from emilia import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7cd17e6f15a68275a33a5.jpg"
+PHOTO = "https://telegra.ph/file/8142e7aac030eebd40a4e.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**✧ Halo [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emilia.** \n\n"
   TEXT += "**✧ Saya Bekerja Dengan Benar** \n\n"
-  TEXT += f"✧ **My Master : [Zenitsu Prjkt](https://t.me/ZenitsuPrjkt)** \n\n"
+  TEXT += f"✧ **Dikelola Oleh : [Zenitsu Prjkt](https://t.me/ZenitsuPrjkt)** \n\n"
   TEXT += f"✧ **Versi Library  :** `{telever}` \n\n"
   TEXT += f"✧ **Versi Telethon :** `{tlhver}` \n\n"
   TEXT += f"✧ **Versi Pyrogram :** `{pyrover}` \n\n"
-  BUTTON = [[Button.url("Bantuan", "https://t.me/EmiliaPrjkt_bot?start=help"), Button.url("Repo", "https://github.com/ZenitsuPrjkt/Emilia")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT)
