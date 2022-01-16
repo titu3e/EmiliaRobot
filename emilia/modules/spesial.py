7c013feae53a4e874e62e89f84858b9f1acdb00f
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-16 12:50:39 +0700

    Create spesial.py

---
+++ emilia/modules/spesial.py
@@ -0,0 +1,44 @@
+__mod_name__ = "Spesial"
+
+__help__ = """
+✦ *Bot Info Sangmata*
+ ✧ /sg <reply>*:* Untuk memeriksa nama riwayat
+
+✦ *Tanggal-waktu-Cuaca*
+ ✧ /time <kode negara>*:* Memberikan informasi tentang zona waktu.
+ ✧ /weather <kota>*:* Memberikan informasi tentang zona waktu..
+ ✧ /wttr <kota>*:* Modul cuaca lanjutan, penggunaan sama seperti /weather
+ ✧ /wttr moon*:* Dapatkan status bulan saat ini
+
+✦ *mengkonversi*
+ ✧ /encrypt*:* Mengenkripsi Teks yang Diberikan
+ ✧ /decrypt*:* Mendekripsi Teks Terenkripsi Sebelumnya
+ ✧ /zip*:* membalas file telegram untuk mengompresnya dalam format .zip
+ ✧ /unzip*:* membalas file telegram untuk mendekompresnya dari format .zip
+
+✦ *Random API*
+ ✧ /ptl atau /asupan: dapatkan video acak dari timeline Instagram
+ ✧ /chika: dapatkan video acak dari chikakiku
+ ✧ /wibu: dapatkan video atau foto anime pendek acak
+
+✦ *Tautan Ke File:*
+ ✧ /up: balas tautan unduhan langsung untuk mengunggahnya ke telegram sebagai file
+ 
+✦ *File Ke Tautan:*
+ ✧ /transfersh: balas file telegram untuk mengunggahnya transfersh dan dapatkan tautan unduhan langsung
+ ✧ /tmpninja: balas file telegram untuk mengunggahnya di tmpninja dan dapatkan tautan unduhan langsung
+
+✦ *File Atau Teks Ke Telegraph:*
+ ✧ /tgm : dapatkan Telegraph Tautan Media yang Dibalas
+ ✧ /tgt: dapatkan Telegraph Tautan Teks yang Dibalas
+
+✦ *Tandai Semua:*
+ ✧ /tagall : untuk tag semua orang minimal 100 member dulu.
+
+✦ *Logo:*
+ ✧ /logo <teks/nama> - Buat logo dengan tampilan acak.
+ ✧ /wlogo <teks/nama> - Buat logo dengan tampilan lebar saja.
+
+✦ *Editor Gambar:*
+ ✧ /edit <balas foto> - untuk mengedit gambar.
+"""
