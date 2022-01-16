cf0098bfe3aff8e67e74d2e9beeea5ba19e720be
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-16 22:03:51 +0700

    Add files via upload

--- emilia/modules/dbcleanup.py
+++ emilia/modules/dbcleanup.py
@@ -90,16 +90,16 @@ def get_invalid_gban(update: Update, context: CallbackContext, remove: bool = Fa
 def dbcleanup(update: Update, context: CallbackContext):
     msg = update.effective_message
 
-    msg.reply_text("Getting invalid chat count ...")
+    msg.reply_text("Mendapatkan jumlah obrolan yang tidak valid...")
     invalid_chat_count = get_invalid_chats(update, context)
 
-    msg.reply_text("Getting invalid gbanned count ...")
+    msg.reply_text("Mendapatkan jumlah gbanned yang tidak valid...")
     invalid_gban_count = get_invalid_gban(update, context)
 
-    reply = f"Total invalid chats - {invalid_chat_count}\n"
-    reply += f"Total invalid gbanned users - {invalid_gban_count}"
+    reply = f"Total obrolan tidak valid - {invalid_chat_count}\n"
+    reply += f"Total pengguna larangan globlal tidak valid - {invalid_gban_count}"
 
-    buttons = [[InlineKeyboardButton("Cleanup DB", callback_data="db_cleanup")]]
+    buttons = [[InlineKeyboardButton("Pembersihan DB", callback_data="db_cleanup")]]
 
     update.effective_message.reply_text(
         reply,
@@ -120,23 +120,23 @@ def callback_button(update: Update, context: CallbackContext):
 
     if query_type == "db_leave_chat":
         if query.from_user.id in admin_list:
-            bot.editMessageText("Leaving chats ...", chat_id, message.message_id)
+            bot.editMessageText("Meninggalkan obrolan...", chat_id, message.message_id)
             chat_count = get_muted_chats(update, context, True)
-            bot.sendMessage(chat_id, f"Left {chat_count} chats.")
+            bot.sendMessage(chat_id, f"Left {chat_count} obrolan.")
         else:
-            query.answer("You are not allowed to use this.")
+            query.answer("Anda tidak diizinkan untuk menggunakan ini.")
     elif query_type == "db_cleanup":
         if query.from_user.id in admin_list:
-            bot.editMessageText("Cleaning up DB ...", chat_id, message.message_id)
+            bot.editMessageText("Membersihkan DB...", chat_id, message.message_id)
             invalid_chat_count = get_invalid_chats(update, context, True)
             invalid_gban_count = get_invalid_gban(update, context, True)
-            reply = "Cleaned up {} chats and {} gbanned users from db.".format(
+            reply = "Dibersihkan {} obrolan dan {} pengguna larangan global dari db.".format(
                 invalid_chat_count,
                 invalid_gban_count,
             )
             bot.sendMessage(chat_id, reply)
         else:
-            query.answer("You are not allowed to use this.")
+            query.answer("Anda tidak diizinkan untuk menggunakan ini.")
 
 
 DB_CLEANUP_HANDLER = CommandHandler("dbcleanup", dbcleanup, run_async=True)
