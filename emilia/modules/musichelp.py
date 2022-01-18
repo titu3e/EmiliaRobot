dbe7e1f579de9593069656a733ad351d23b7af48
Zenitsu Prjkt™ <85629049+ZenitsuPrjkt@users.noreply.github.com>
2022-01-18 18:27:40 +0700

    Delete emilia directory

--- emilia/modules/musichelp.py
+++
@@ -1,8 +0,0 @@
-from emilia.modules.language import gs
-
-
-def helps(chat):
-    return gs(chat, "musicplayer_help")
-
-
-__mod_name__ = "Musicplayer"
--- emilia/modules/tools.py
+++
@@ -1,8 +0,0 @@
-from emilia.modules.language import gs
-
-
-def helps(chat):
-    return gs(chat, "tools_help")
-
-
-__mod_name__ = "Tools"
